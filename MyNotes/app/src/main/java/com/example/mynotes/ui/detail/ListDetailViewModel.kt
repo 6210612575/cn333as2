5e36e146e2d7a23dc7a38b9e9a0e37cadfcd94a6
Anudchana Panyapradit <6210612575@cn331>
2022-02-25 01:28:45 +0700

    delete mynote because error

--- MyNotes/app/src/main/java/com/example/mynotes/ui/detail/ListDetailViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mynotes.ui.detail
-
-import androidx.lifecycle.ViewModel
-import com.example.mynotes.models.NoteDetail
-
-class ListDetailViewModel : ViewModel() {
-
-}
--- MyNotes/app/src/main/java/com/example/mynotes/ui/main/NoteSelectionViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mynotes.ui.main
-
-import androidx.recyclerview.widget.RecyclerView
-import com.example.mynotes.databinding.NoteSelectionViewHolderBinding
-
-class NoteSelectionViewHolder(val binding: NoteSelectionViewHolderBinding): RecyclerView.ViewHolder(binding.root) {
-
-}
